3575b1cfca7ed134f108793aa2719bb3e08e0fee
zcy <179363642@qq.com>
2019-09-19 16:21:40 +0800

    第2次提交

--- src/main/java/net/test/demo01.java
+++ src/main/java/net/test/demo01.java
@@ -3,10 +3,6 @@ package net.test;
 public class demo01 {
     public static void main(String[] args) {
         System.out.println("hello");System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
 
     }
 }
