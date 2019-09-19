6a1c761e577e6cf2c1f6a39409402cb4f6583f95
zcy <179363642@qq.com>
2019-09-19 16:09:03 +0800

    第一次提交

---
+++ src/main/java/net/test/teest.java
@@ -0,0 +1,14 @@
+package net.test;
+
+public class teest {
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println("11");
+        System.out.println("sdfsdf");
+        System.out.println("asd2222");
+        System.out.println(123);
+
+    }
+}
